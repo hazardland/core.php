05b407551a628fa93b9f9f8421e028d3845ac069
hazardland <hazardland@gmail.com>
2017-03-17 20:06:46 +0400

    reView

--- src/View.php
+++ src/View.php
@@ -11,12 +11,19 @@
             self::$path = $path;
             self::$extension = $extension;
         }
-        public static function render ($name, $values=[])
+        public static function render ($__name, $__values=[])
         {
             if (self::$path===null)
             {
                 trigger_error("[\\Core\\View] view path is not defined", E_USER_ERROR);
             }
-            include self::$path.'/'.str_replace(['/','\\'],'',$name).self::$extension;
+            if (is_array($__values) && count($__values))
+            {
+                foreach ($__values as $__key => &$__value)
+                {
+                    ${$__key} = $__value;
+                }
+            }
+            include self::$path.'/'.str_replace(['/','\\'],'',$__name).self::$extension;
         }
     }
